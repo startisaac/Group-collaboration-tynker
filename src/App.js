81af810c9972beccb4a92adff2681035e9b8868d
startisaac <61977638+startisaac@users.noreply.github.com>
2022-03-05 23:43:44 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,229 @@
+
+import react from "react"
+import './App.css';
+import apple from './Image/image pro/apple.PNG'
+import google from './Image/image pro/google.PNG'
+import microsoft from './Image/image pro/microsoft.PNG'
+import pbs from './Image/image pro/pbs.PNG'
+import bbc from './Image/image pro/bbc.PNG'
+import fb from './Image/image pro/fb.PNG'
+import lego from './Image/image pro/lego.PNG'
+import minecraft from './Image/image pro/minecraft.PNG'
+import star from './Image/image pro/star.PNG'
+import wheels from './Image/image pro/wheels.PNG'
+import barbli from './Image/image pro/barbli.PNG'
+import commonsense from './Image/image pro/commonsense.PNG'
+import AASL from './Image/image pro/AASL.PNG'
+import smartmedia from './Image/image pro/smartmedia.PNG'
+import parentchoice from './Image/image pro/parentchoice.PNG'
+import brainchild from './Image/image pro/brainchild.PNG'
+import national from './Image/image pro/national.PNG'
+import momchoice from './Image/image pro/momchoice.PNG'
+import digital from './Image/image pro/digital.PNG'
+import parentpick from './Image/image pro/parentpick.PNG'
+import madam from './Image/image pro/madam.png'
+import tynker from './Image/image pro/tynker.png'
+import valeries from './Image/image pro/valeries.png'
+import map from './Image/image pro/map-blue.png'
+import designtop from './Image/image pro/bg-squiggle-top.svg'
+import cloud from './Image/image pro/cloud.jpeg'
+import dots from './Image/image pro/dots.svg'
+import comma from './Image/image pro/comma.svg'
+import over100 from './Image/image pro/over100.PNG'
+import million from './Image/image pro/60million.PNG'
+import billion from './Image/image pro/7million.PNG'
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+
+      <div className="writeup">
+        <img src={designtop} className="designtop" />
+
+        <div className="why_kid_code">
+          <h2 className="center_text">Why Should Kids Learn to Code with Tynker?</h2>
+          <p className="kid_code">Because <b>technology is so integrated</b> into virtually every aspect of our lives, learning about it is <b>more important than ever</b>. For kids, our coding platform makes it convenient and fun to gain knowledge that will serve them well throughout their lives. You can count on our <b>trusted methods</b> to help them develop and grow their capabilities with computers.</p>
+
+          <p className="kid_code">Tynker isn’t just for schools and coding camps, though. Parents who value <b>STEM education</b> and want their children to know how to learn to code should know that our kids programming platform can be used at home, too. There are individual and family home plans, and our online coding classes for kids are a great way for them to gain the supplemental STEM skills needed for the world of the future.</p>
+        </div>
+
+
+        <div className="card_wrapper">
+          <div className="card_writeup">
+            <h3 className="h3_color">CODING BENEFITS</h3>
+            <ul>
+              <li>Top 10 Reasons to Code</li>
+              <li>Kids Coding Basics</li>
+              <li>Develop 21st Century Skills</li>
+              <li>Download eBooks on Coding</li>
+            </ul>
+          </div>
+          <div className="card_writeup">
+            <h3 className="h3_color">CODING GUIDES</h3>
+            <ul>
+              <li>What is Coding for Kids</li>
+              <li>Coding with Minecraft</li>
+              <li>Coding with Electronic Kits</li>
+              <li>Coding with Connected Toys</li>
+              <li>More Coding Resources</li>
+            </ul>
+          </div>
+          <div className="card_writeup">
+            <h3 className="h3_color">TYNKER HIGHLIGHTS</h3>
+            <ul>
+              <li>What’s New in Tynker?</li>
+              <li>COVID-19 Impact report</li>
+              <li>Meet our Featured Makers</li>
+              <li>Seasonal Coding Projects</li>
+              <li>Celebrating Women in STEM</li>
+            </ul>
+          </div>
+          <div className="card_writeup">
+            <h3 className="h3_color">CODING IN SCHOOLS</h3>
+            <ul>
+              <li>K-12 School Plans</li>
+              <li>AP Computer Science Principles</li>
+              <li>AP Computer Science A</li>
+              <li>emote Learning</li>
+              <li>K-12 Success Stories</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="why_kid_code">
+          <h2 className="center_text">Trusted by Leading Brands</h2>
+          <p>Our methods for learning to code all are provided within a safe, moderated community that is built around encouraging students to gain confidence in their abilities and do so without fear of failure.</p>
+
+        </div>
+
+        <div className="logos_wrapper1">
+          <img src={apple} alt="logo" className="apple" />
+          <img src={google} className="apple" />
+          <img src={microsoft} className="apple" />
+          <img src={pbs} className="apple" />
+          <img src={bbc} className="apple" />
+          <img src={fb} className="apple" />
+        </div>
+
+        <div className="logos_wrapper2">
+          <img src={lego} alt="logo" className="apple" />
+          <img src={minecraft} className="apple" />
+          <img src={star} className="apple" />
+          <img src={wheels} className="apple" />
+          <img src={barbli} className="apple" />
+        </div>
+
+        <div className="join_tynkerwrapper">
+          <div className="join_tynker">
+            <h2>Join Tynker's Global Community</h2>
+            <button className="but_signup">SIGN UP FREE</button>
+          </div>
+
+          <img src={map} className="my_map" />
+
+
+
+        </div>
+        <br /><br />
+        <img src={cloud} className="my_cloud" />
+
+        <div className="over7billion">
+          {/* <h6><b>over 7 <br />billion</b></h6><br /> */}
+
+        </div>
+
+        <div className="over100_wrapper">
+          <div className="over100">
+            <h6><b>over 100 <br />Thousand<br />schools</b></h6><br />
+
+          </div>
+        </div>
+        <img src={dots} className="dot" />
+        <img src={dots} className="dot1" />
+        <img src={dots} className="dot2" />
+
+
+
+
+
+
+
+        <div className="wrapper_container">
+          <div className="card_wrapper">
+            <div className="card1">
+              <p><img src={comma} className="comma" />I thought a game was going to be extremely hard to make, but Tynker made it really easy.</p>
+              <div className="writeupix">
+                <p className="p_f"><b>Tynker Featured<br /> Maker</b></p>
+                <img src={tynker} className="madam_pix" />
+              </div>
+            </div>
+
+            <div className="card2">
+              <p ><img src={comma} className="comma" />There's so much opportunity once they understand how coding works. It's amazing to see them create incredible things themselves.</p>
+              <div className="writeupix">
+                <p className="p_f"><b>Charlotte R.</b><br />Parent | Melbourne,<br /> Australia</p>
+                <img src={madam} className="madam_pix" />
+              </div>
+            </div>
+
+            <div className="card3">
+              <p><img src={comma} className="comma" />Tynker has made programming fun, exciting, and simple and will help lead kids into their future.</p>
+              <div className="writeupix">
+                <p className="p_f"><b>Valerie S.</b><br />5th Grade Teacher | California, US</p>
+                <img src={valeries} className="madam_pix" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src={over100} className="over100" />
+        <img src={million} className="million" />
+        <img src={billion} className="billion" />
+
+
+
+
+        <div className="logos_wrapper3">
+          <div className="our_logos">
+            <img src={commonsense} />
+            <img src={AASL} />
+            <img src={smartmedia} />
+            <img src={parentchoice} />
+            <img src={brainchild} />
+            <img src={national} />
+            <img src={momchoice} />
+            <img src={digital} />
+            <img src={parentpick} />
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+
+    </div >
+
+  )
+}
+
+export default App
+
+
+
